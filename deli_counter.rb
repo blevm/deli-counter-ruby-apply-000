050598b62bcf472975ab9c803f643b1631b018ec
Michelle B <mjblevins7@gmail.com>
2018-02-03 01:38:05 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,13 @@
 # Write your code here.
 katz_deli = []
 
-#def line(array)
-#  if katz_deli.length = 0
-#    puts "The line is currently empty."
-#  else
-#    array.each do |person|
-#    end
-#  end
-#end
+def line(array)
+  if array.length = 0
+   puts "The line is currently empty."
+  else
+   array.each {|person| puts "#{array.find_index(person)+1}. #{person}"}
+  end
+end
 
 def take_a_number(array, name)
   array.push(name)
