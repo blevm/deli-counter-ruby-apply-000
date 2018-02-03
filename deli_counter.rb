4be5a4b5a0a45fe4477492a57b76ae99727fc01a
Michelle B <mjblevins7@gmail.com>
2018-02-03 01:50:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,6 +23,6 @@ def now_serving(array)
    puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{array.first}."
-    array.unshift(array.first)
+    array.shift(array.first)
   end
 end
