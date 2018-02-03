78085bf61a3bc70fee702df80538005f4d606272
Michelle B <mjblevins7@gmail.com>
2018-02-03 01:21:56 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,9 +10,9 @@ def take_a_number
 end
 
 def now_serving
-  if katz_deli.length !== 0
+  if katz_deli.length !=== 0
   puts "Next person in line is #{line}"
-  elsif katz_deli.length == 0
+elsif katz_deli.length === 0
     puts "There is nobody waiting to be served!"
   else
 end
