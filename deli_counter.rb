15bc56f986e67dc7be06a7f5ee6c73e0ba806352
Michelle B <mjblevins7@gmail.com>
2018-02-03 01:31:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,24 +1,24 @@
 # Write your code here.
 katz_deli = []
 
-def line(array)
-  if katz_deli.length = 0
-    puts "The line is currently empty."
-  else
-    array.each do |person|
-    end
-  end
-end
+#def line(array)
+#  if katz_deli.length = 0
+#    puts "The line is currently empty."
+#  else
+#    array.each do |person|
+#    end
+#  end
+#end
 
 def take_a_number(array, name)
   array.push(name)
   puts "Welcome, #{name}. You are number #{array.find_index("name")+1} in line."
 end
 
-def now_serving
-  if katz_deli.length != 0
-  puts "Next person in line is #{line}"
-  elsif katz_deli.length = 0
-    puts "There is nobody waiting to be served!"
-  end
-end
+#def now_serving
+#  if katz_deli.length != 0
+#  puts "Next person in line is #{line}"
+#  elsif katz_deli.length = 0
+#    puts "There is nobody waiting to be served!"
+#  end
+#end
