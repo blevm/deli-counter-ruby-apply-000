337ea69d7524532ade03ec1ed5d7d25c55fb77cd
Michelle B <mjblevins7@gmail.com>
2018-02-03 01:21:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+katz_deli = []
+
+def line
+
+end
+
+def take_a_number
+
+end
+
+def now_serving
+  if katz_deli.length !== 0
+  puts "Next person in line is #{line}"
+  elsif katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+
+end
+
+
